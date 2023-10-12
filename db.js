70a7a3594a8d8252757a1bd7a0ce0f30ae659d60
Lara Lopes <105398178+LaraAlexLopes@users.noreply.github.com>
2023-10-12 12:22:57 -0300

    Create db.js

---
+++ db.js
@@ -0,0 +1,20 @@
+{
+  "fragrancia":[
+    {"descricao": "Salvia"},
+    {"descricao": "Floral"},
+    {"descricao": "Morango"},
+    {"descricao": "Tangerina"}
+  ],
+  "tamanho":[
+    {"volume": "50 ml"},
+    {"volume": "100 ml"},
+    {"volume": "50 ml"},
+    {"volume": "200 ml"}
+  ],
+  "listaPedido":[
+    {"nome": "Salvia","quantidade": "50 ml","valor": "R$ 50"},
+    {"nome": "Tangerina","quantidade": "200 ml","valor":"R$ 200"},
+    {"nome": "Floral","quantidade": "100 ml","valor":"R$ 100"},
+    {"nome": "Morango","quantidade": "50 ml","valor":"R$ 200"}
+  ]
+}
